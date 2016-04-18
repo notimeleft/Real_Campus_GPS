8314ed127e0982c073936aa6ffbbfffa5b036121
seanx92 <dummyshooter@gmail.com>
2016-04-18 16:53:19 -0400

    compatible with safari

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,9 +5,3 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( jquery-1.11.1.min.js )
-Rails.application.config.assets.precompile += %w( bootstrap.min.js )
-Rails.application.config.assets.precompile += %w( jquery.backstretch.min.js )
-Rails.application.config.assets.precompile += %w( scripts.js )
-#Rails.application.config.assets.precompile += %w( form-elements.css )
-#Rails.application.config.assets.precompile += %w( welcome.css )
